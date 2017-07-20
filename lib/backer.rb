03bbfcb9ee6fe41de31af778eb7057f58935d0a6
lglayat <glayatle@g.cofc.edu>
2017-07-20 13:34:21 -0400

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,22 @@
+class Backer
+
+  attr_accessor :backed_projects
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+
+  end
+
+  def back_project(project)
+    self.backed_projects << project
+    project.add_backer(self)
+  end
+
+  def backed_projects
+    @backed_projects
+  end
+
+
+end
